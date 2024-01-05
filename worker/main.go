5070a31f7d812c5c5dddc581e00d0ec9010568ac
Michee <allidjinoumichee@outlook.fr>
2024-01-05 19:49:07 +0100

    feat(worker): receive messages from the task queue
    Worker can receive messages from the task queue.

--- worker/main.go
+++ worker/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 // TODO: get name from the backend server
@@ -24,4 +26,41 @@ func main() {
 	log.Printf("argument `rabbitmq` set to %s\n", RABBITMQ_URL)
 
 	// TODO: listen to messages from the queue.
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	FailOnError(err, "failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	FailOnError(err, "failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		TASK_QUEUE_NAME, // name
+		false,           // durable
+		false,           // delete when unused
+		false,           // exclusive
+		false,           // no-wait
+		nil,             // arguments
+	)
+	tasks, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	FailOnError(err, "failed to register a consumer")
+
+	var forever chan struct{}
+
+	go func() {
+		for task := range tasks {
+			log.Printf("[%s] received a message: %s\n", WORKER_NAME, task.Body)
+		}
+	}()
+
+	log.Printf("[%s] waiting for messages. To exit press CTRL+C\n", WORKER_NAME)
+	<-forever
 }
