5ead5f24aedcb2e9f3ac5227da3ed4246b8e6827
Michee <allidjinoumichee@outlook.fr>
2024-01-05 18:21:03 +0100

    feat: create http server
    Create the http server.
    Add function to handle request to root url.

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+
+func HandleRoot(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		log.Printf("%s /\n", r.Method)
+	// An empty method corresponds to a GET response for a client.
+	case "":
+		log.Printf("%s /\n", r.Method)
+	default:
+		log.Printf("%s / unsupported method\n", r.Method)
+		io.WriteString(w, "error")
+		return
+	}
+
+	io.WriteString(w, "ok")
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", HandleRoot)
+
+	err := http.ListenAndServe(":3333", nil)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		log.Println("server closed")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
 }
