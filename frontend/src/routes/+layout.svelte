669cc9630d8b9e9e6e022c5983dbb72226cd789b
Michée Allidjinou <allidjinoumichee@outlook.fr>
2024-01-06 19:44:41 +0100

    feat(frontend): add global layout

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script>
 	import '../app.css';
+	import Sidebar from '../components/Sidebar.svelte';
 </script>
 
-<slot />
+<div class="flex">
+	<Sidebar />
+	<main
+		class="flex flex-col items-center gap-8 bg-gray-100 rounded-xl p-4 my-12 mx-4 w-full min-h-[700px]"
+	>
+		<h1 class="text-4xl text-center font-semibold">silverdog</h1>
+
+		<slot />
+	</main>
+</div>
