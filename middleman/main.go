4d3c2055863b456a3e6b8df25e4edb124f242ec4
Michée Allidjinou <allidjinoumichee@outlook.fr>
2024-01-07 22:26:09 +0100

    feat(middleman): parse command line arguments
    Get RabbitMQ connection URL from the command line.
    Get listening port from the command line.
    Add default values for command line arguments.

--- middleman/main.go
+++ middleman/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+)
+
+const DEFAULT_PORT int = 8090
+const DEFAULT_RABBITMQ_URL string = "amqp://guest:guest@localhost:5672/"
+const TASK_QUEUE_NAME string = "tasks"
+const NOTIF_QUEUE_NAME string = "notifications"
+
+var PORT int
+var RABBITMQ_URL string
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	flag.IntVar(&PORT, "port", DEFAULT_PORT, "the listening port for the server")
+	flag.StringVar(&RABBITMQ_URL, "rabbitmq", DEFAULT_RABBITMQ_URL, "the connection url to RabbitMQ")
+
+	flag.Parse()
+
+	log.Printf("argument `port` set to %d\n", PORT)
+	log.Printf("argument `rabbitmq` set to %s\n", RABBITMQ_URL)
 }
