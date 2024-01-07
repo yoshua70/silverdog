a274b06e6ffbbb658a52d0d7ca65d66435ecf10d
Michée Allidjinou <allidjinoumichee@outlook.fr>
2024-01-07 22:57:02 +0100

    feat(worker): add function to send messages to a RabbitMQ queue

--- worker/helpers.go
+++ worker/helpers.go
@@ -1,11 +1,53 @@
 package main
 
 import (
+	"context"
 	"log"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func FailOnError(err error, msg string) {
 	if err != nil {
 		log.Panicf("%s: %s", msg, err)
 	}
+}
+
+func SendMessageToQeue(queueName string, body []byte) {
+
+	conn, err := amqp.Dial(RABBITMQ_URL)
+	FailOnError(err, "failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	FailOnError(err, "failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		queueName, // name
+		false,     // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
+	)
+	FailOnError(err, "failed to declare a queue")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = ch.PublishWithContext(ctx,
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			DeliveryMode: amqp.Persistent,
+			ContentType:  "text/plain",
+			Body:         body,
+		})
+
+	// TODO: return the error instead of panicking.
+	FailOnError(err, "failed to publish a message")
 }
