3369cf47c412a93aa84e60dd50fcc22b66963d4b
Michée Allidjinou <allidjinoumichee@outlook.fr>
2024-01-07 22:51:21 +0100

    feat(middleman): complete first iteration
    Listen from notifications queue.
    Store notification messages locally.
    Send stored notifications each x seconds.

--- middleman/main.go
+++ middleman/main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gorilla/websocket"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+const REFRESH_INTERVAL int = 5
 const DEFAULT_PORT int = 8090
 const DEFAULT_RABBITMQ_URL string = "amqp://guest:guest@localhost:5672/"
 const TASK_QUEUE_NAME string = "tasks"
@@ -19,10 +23,12 @@ const NOTIF_QUEUE_NAME string = "notifications"
 type Message struct {
 	Status string `json:"status"`
 	Body   string `json:"body"`
+	Sent   bool   `json:"sent"`
 }
 
 var PORT int
 var RABBITMQ_URL string
+var messages []Message
 
 // Upgrade a regular HTTP connection to a websocket connection.
 var httpToWebSocketUpgrader = websocket.Upgrader{
@@ -31,8 +37,92 @@ var httpToWebSocketUpgrader = websocket.Upgrader{
 	},
 }
 
-func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+func consummer() {
+	conn, err := amqp.Dial(RABBITMQ_URL)
+	FailOnError(err, "failed to connect to RabbitMQ")
+	defer conn.Close()
 
+	ch, err := conn.Channel()
+	FailOnError(err, "failed to open a channel")
+	defer ch.Close()
+
+	_, err = ch.QueueDeclare(
+		NOTIF_QUEUE_NAME, // name
+		false,            // durable
+		false,            // delete when unused
+		false,            // exclusive
+		false,            // no-wait
+		nil,              // arguments
+	)
+	FailOnError(err, "failed to declare a queue")
+
+	messages, err := ch.Consume(
+		NOTIF_QUEUE_NAME,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		log.Printf("failed to register consummer for queue %s: %s\n", NOTIF_QUEUE_NAME, err)
+		return
+	}
+
+	for message := range messages {
+		storeMessage(message)
+	}
+}
+
+func storeMessage(message amqp.Delivery) {
+	msg, err := messageParser(string(message.Body))
+
+	if err == nil {
+		log.Printf("store message: %v\n", msg)
+		messages = append(messages, msg)
+	}
+}
+
+func messageParser(message string) (Message, error) {
+	msg := Message{Sent: false}
+	err := json.Unmarshal([]byte(message), &msg)
+
+	if err != nil {
+		log.Printf("failed to decode message: %s\n", err)
+		return Message{}, err
+	}
+
+	return msg, nil
+}
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := httpToWebSocketUpgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Printf("failed to upgrade http connection to websocket: %s\n", err)
+		return
+	}
+
+	defer conn.Close()
+
+	for {
+		time.Sleep(time.Second * time.Duration(REFRESH_INTERVAL))
+
+		for i, message := range messages {
+			if !message.Sent {
+				err := conn.WriteJSON(message)
+
+				if err != nil {
+					log.Printf("failed to write json in websocket pipe: %s\n", err)
+					return
+				}
+				log.Printf("sent message: %s\n", message.Body)
+				messages[i] = Message{Body: message.Body, Status: message.Status, Sent: true}
+			}
+		}
+	}
 }
 
 func main() {
