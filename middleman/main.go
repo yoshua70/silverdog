64e9852cd761bdfddf7bdb6f14972b33affd8367
Michée Allidjinou <allidjinoumichee@outlook.fr>
2024-01-07 22:34:45 +0100

    feat(middleman): set up http server
    Add handle function for websocket connection.
    Add amqp and gorilla packages.
    Add upgrader for http connections.

--- middleman/main.go
+++ middleman/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"fmt"
 	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/websocket"
 )
 
 const DEFAULT_PORT int = 8090
@@ -10,8 +16,24 @@ const DEFAULT_RABBITMQ_URL string = "amqp://guest:guest@localhost:5672/"
 const TASK_QUEUE_NAME string = "tasks"
 const NOTIF_QUEUE_NAME string = "notifications"
 
+type Message struct {
+	Status string `json:"status"`
+	Body   string `json:"body"`
+}
+
 var PORT int
 var RABBITMQ_URL string
+
+// Upgrade a regular HTTP connection to a websocket connection.
+var httpToWebSocketUpgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+
+}
 
 func main() {
 
@@ -22,4 +44,16 @@ func main() {
 
 	log.Printf("argument `port` set to %d\n", PORT)
 	log.Printf("argument `rabbitmq` set to %s\n", RABBITMQ_URL)
+
+	http.HandleFunc("/ws", handleWebSocket)
+	log.Printf("websoket running on: %d\n", PORT)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		log.Println("server closed")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
 }
